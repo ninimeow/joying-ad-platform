4d127f336dfc31954f382de4ebd7e82caed5ee88
liqian <quanshang123@qq.com>
2020-02-11 17:00:00 +0800

    页面布局调整

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,29 @@
 
 <template>
   <div class="layout">
-    <Layout>
-      <Header>
-        <Menu mode="horizontal" theme="dark" active-name="1">
-          <div class="layout-nav">
-            <MenuItem name="4">
-              <Icon type="ios-contact" />管理员
-            </MenuItem>
-          </div>
-        </Menu>
+    <Sider :style="{position: 'fixed', height: '100vh', left: 0, overflow: 'auto'}">
+      <div class="logo"></div>
+      <Menu active-name="1-2" theme="dark" width="auto" :open-names="['1']">
+        <Submenu name="1">
+          <template slot="title">
+            <Icon type="ios-navigate"></Icon>营销活动管理
+          </template>
+          <MenuItem name="1-1">EDM</MenuItem>
+          <MenuItem name="1-2">营销活动</MenuItem>
+        </Submenu>
+      </Menu>
+    </Sider>
+    <Layout :style="{marginLeft: '200px'}">
+      <Header class="header">
+        <div>
+          <Icon type="ios-contact" />管理员
+        </div>
       </Header>
-      <Layout>
-        <Sider hide-trigger :style="{background: '#fff'}">
-          <Menu active-name="1-2" theme="light" width="auto" :open-names="['1']">
-            <Submenu name="1">
-              <template slot="title">
-                <Icon type="ios-navigate"></Icon>营销活动
-              </template>
-              <MenuItem name="1-1">EDM</MenuItem>
-              <MenuItem name="1-2">营销页面</MenuItem>
-            </Submenu>
-          </Menu>
-        </Sider>
-      </Layout>
+      <Content>
+        <Card>
+          <div style="height: 600px">Content</div>
+        </Card>
+      </Content>
     </Layout>
   </div>
 </template>
@@ -36,15 +36,24 @@ export default {
 
 <style scoped>
 .layout {
-  border: 1px solid #d7dde4;
   background: #f5f7f9;
   position: relative;
   border-radius: 4px;
   overflow: hidden;
 }
-.layout-nav {
-  margin: 0 auto;
-  margin-right: 20px;
-  float: right;
+.layout-header-bar {
+  background: #fff;
+  box-shadow: 0 1px 1px rgba(0, 0, 0, 0.1);
+}
+
+.header {
+  background: #fff;
+}
+
+.logo {
+  height: 63px;
+  line-height: 63px;
+  text-align: center;
+  overflow: hidden;
 }
 </style>
