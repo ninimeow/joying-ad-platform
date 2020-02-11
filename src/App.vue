6b51effa57659b97c57e35928dc6a5f83e189e6f
liqian <quanshang123@qq.com>
2020-02-11 16:40:32 +0800

    页面布局

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,30 @@
+
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+  <div class="layout">
+    <Layout>
+      <Header>
+        <Menu mode="horizontal" theme="dark" active-name="1">
+          <div class="layout-nav">
+            <MenuItem name="4">
+              <Icon type="ios-contact" />管理员
+            </MenuItem>
+          </div>
+        </Menu>
+      </Header>
+      <Layout>
+        <Sider hide-trigger :style="{background: '#fff'}">
+          <Menu active-name="1-2" theme="light" width="auto" :open-names="['1']">
+            <Submenu name="1">
+              <template slot="title">
+                <Icon type="ios-navigate"></Icon>营销活动
+              </template>
+              <MenuItem name="1-1">EDM</MenuItem>
+              <MenuItem name="1-2">营销页面</MenuItem>
+            </Submenu>
+          </Menu>
+        </Sider>
+      </Layout>
+    </Layout>
   </div>
 </template>
 
@@ -11,13 +34,17 @@ export default {
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+.layout {
+  border: 1px solid #d7dde4;
+  background: #f5f7f9;
+  position: relative;
+  border-radius: 4px;
+  overflow: hidden;
+}
+.layout-nav {
+  margin: 0 auto;
+  margin-right: 20px;
+  float: right;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,11 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import ViewUI from 'view-design'
+
+import 'view-design/dist/styles/iview.css'
+
+Vue.use(ViewUI)
 
 Vue.config.productionTip = false
 
@@ -10,6 +15,8 @@ Vue.config.productionTip = false
 new Vue({
   el: '#app',
   router,
-  components: { App },
+  components: {
+    App
+  },
   template: '<App/>'
 })
