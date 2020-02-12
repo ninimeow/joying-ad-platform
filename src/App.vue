babbaf06c3c583ecef41ac3b4184d0a53d0e5bd5
盛天霓 <13510774@qq.com>
2020-02-12 12:24:31 +0800

    侧边和顶部导航样式调整。

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,38 @@
 
 <template>
   <div class="layout">
-    <Sider :style="{position: 'fixed', height: '100vh', left: 0, overflow: 'auto'}">
-      <div class="logo"></div>
-      <Menu active-name="1-2" theme="dark" width="auto" :open-names="['1']">
-        <Submenu name="1">
-          <template slot="title">
-            <Icon type="ios-navigate"></Icon>营销活动管理
-          </template>
-          <MenuItem name="1-1">EDM</MenuItem>
-          <MenuItem name="1-2">营销活动</MenuItem>
-        </Submenu>
-      </Menu>
+    <Sider ref="side1" hide-trigger collapsible :collapsed-width="78" v-model="isCollapsed" :width="256" >
+      <div class="sider">
+        <div class="logo"></div>
+        <Menu active-name="1-2" theme="dark" width="auto" @on-select="select" :class="menuitemClasses">
+          <Submenu name="1">
+            <template slot="title">
+              <Icon type="ios-apps"></Icon><span>营销活动管理</span>
+            </template>
+            <div class="sub-menu">
+              <MenuItem name="1-1">EDM</MenuItem>
+              <MenuItem name="1-2">营销活动</MenuItem>
+            </div>
+          </Submenu>
+        </Menu>
+      </div>
     </Sider>
-    <Layout :style="{marginLeft: '200px'}">
+    <Layout>
       <Header class="header">
-        <div>
-          <Icon type="ios-contact" />管理员
+        <div class="header-content" :style="{width: headWidth}">
+          <div>
+            <Icon @click.native="collapsedSider" :class="rotateIcon" :style="{margin: '0 20px'}" type="ios-arrow-dropleft" size="24"/>
+            <Breadcrumb>
+              <BreadcrumbItem to="/">Home</BreadcrumbItem>
+              <BreadcrumbItem to="/">Components</BreadcrumbItem>
+              <BreadcrumbItem>Breadcrumb</BreadcrumbItem>
+            </Breadcrumb>
+          </div>
+          <div class="i-layout-header-right">
+            <span>
+              <Icon type="ios-contact" />管理员
+            </span>
+          </div>
         </div>
       </Header>
       <Content>
@@ -30,7 +46,30 @@
 
 <script>
 export default {
-  name: 'App'
+  name: 'App',
+  data () {
+    return {
+      isCollapsed: false
+    }
+  },
+  computed: {
+    rotateIcon () {
+      return ['menu-icon', this.isCollapsed ? 'rotate-icon' : '']
+    },
+    menuitemClasses () {
+      return ['menu-item', this.isCollapsed ? 'collapsed-menu' : '']
+    },
+    headWidth () {
+      return this.isCollapsed ? 'calc(100% - 78px)' : 'calc(100% - 256px)'
+    }
+  },
+  methods: {
+    collapsedSider () {
+      this.$refs.side1.toggleCollapse()
+    },
+    select () {
+    }
+  }
 }
 </script>
 
@@ -40,20 +79,95 @@ export default {
   position: relative;
   border-radius: 4px;
   overflow: hidden;
+  display: flex;
+  justify-content: space-between;
 }
 .layout-header-bar {
   background: #fff;
   box-shadow: 0 1px 1px rgba(0, 0, 0, 0.1);
 }
-
+.ivu-layout-sider, .ivu-menu-dark,
+.ivu-menu-dark.ivu-menu-vertical /deep/ .ivu-menu-opened .ivu-menu-submenu-title,
+.ivu-menu-dark.ivu-menu-vertical /deep/ .ivu-menu-item:hover,
+.ivu-menu-dark.ivu-menu-vertical /deep/ .ivu-menu-submenu-title:hover {
+  background: #191a23;
+}
+.ivu-layout-sider{
+  box-shadow: 2px 0 6px rgba(0,21,41,.35);
+  flex: 0 0 256px;
+}
+.ivu-menu-dark.ivu-menu-vertical .ivu-menu-opened{
+  background: #101117;
+}
+.collapsed-menu span{
+  font-size: 0px;
+}
+.collapsed-menu .ivu-icon{
+  font-size: 24px;
+  transition: font-size .2s ease;
+}
+.collapsed-menu /deep/ .ivu-icon-ios-arrow-down:before{
+  content: "";
+}
+.collapsed-menu .sub-menu{
+  position: absolute;
+  left: 80px;
+  top: -70px;
+  background: #101117;
+  border-radius: 3px;
+  overflow: hidden;
+  width: 150px;
+}
+.collapsed-menu /deep/ .ivu-menu-vertical .ivu-menu-item{
+  padding-left: 15px;
+}
 .header {
   background: #fff;
+  height: 64px;
+  padding: 0;
 }
+.header-content{
+  background: #fff;
+  box-shadow: 0 1px 4px rgba(0,21,41,.08);
+  position: fixed;
+  z-index: 9;
+  display: flex;
+  justify-content: space-between;
 
+}
+.ivu-breadcrumb{
+  margin-left: 20px;
+  display: inline-block;
+}
+.i-layout-header-right {
+  height: 64px;
+  padding-right: 20px;
+}
+.ivu-layout{
+  background: #f5f7f9;
+  flex: auto;
+}
 .logo {
   height: 63px;
   line-height: 63px;
   text-align: center;
   overflow: hidden;
+}
+/deep/ .ivu-card-bordered{
+  border: none;
+  background: none;
+}
+/deep/ .ivu-card-bordered:hover{
+  box-shadow: none;
+}
+/deep/ .ivu-card-body{
+  background: #fff;
+  margin: 20px;
+}
+.menu-icon{
+  transition: all .3s;
+}
+.rotate-icon{
+  transform: rotate(-180deg);
 }
 </style>
