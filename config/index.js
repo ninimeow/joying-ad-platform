da8582929e64d78fc35b5ad9d07842364126d37f
liqian <quanshang123@qq.com>
2020-02-13 14:57:14 +0800

    调整编译资源目录

--- config/index.js
+++ config/index.js
@@ -9,7 +9,7 @@ module.exports = {
 
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/h5',
     proxyTable: {},
 
     // Various Dev Server settings
